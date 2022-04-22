35b7c2d8eb648dcd925cd4258a1b0aaf8dfd3215
Sumanth Lingappa <sumanth.lingappa@citrix.com>
2022-04-22 09:41:43 +0530

    corrected environment variable type

--- examples/data-sources/citrixadm_mps_agent/provider.tf
+++ examples/data-sources/citrixadm_mps_agent/provider.tf
@@ -9,6 +9,6 @@ provider "citrixadm" {
   # host          = "https://adm.cloud.com"                # Optionally use CITRIXADM_HOST env var
   # client_id     = "12345678-90ab-cdef-ghij-klmnopqrstuv" # Optionally use CITRIXADM_CLIENT_ID env var
   # client_secret = "ABcdefgHIJklmnopqrstuv=="             # Optionally use CITRIXADM_CLIENT_SECRET env var
-  # host_location = "us"                                   # us, eu # Optionally use CITRIX_ADM_HOST_LOCATION env var
-  # customer_id   = "abcdefghijkl"                         # Optionally use CITRIX_ADM_CUSTOMER_ID env var
+  # host_location = "us"                                   # us, eu # Optionally use CITRIXADM_HOST_LOCATION env var
+  # customer_id   = "abcdefghijkl"                         # Optionally use CITRIXADM_CUSTOMER_ID env var
 }
