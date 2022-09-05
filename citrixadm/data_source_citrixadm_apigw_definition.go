9b9ae966291eb7fd3b9ed89d837c2b0320ed34ab
Rohit Myali <rohit.myali@citrix.com>
2022-09-05 12:05:37 +0000

    **New Resource** data_source_apigw_definition
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

---
+++ citrixadm/data_source_citrixadm_apigw_definition.go
@@ -0,0 +1,56 @@
+package citrixadm
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"terraform-provider-citrixadm/service"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceApigwDefinition() *schema.Resource {
+	return &schema.Resource{
+		Description: "Get an API Definition Id by name",
+		ReadContext: dataSourceApigwDefinitionRead,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description: "Name of an API Definition instance",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+		},
+	}
+}
+
+func dataSourceApigwDefinitionRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("In dataSourceApigwDefinitionRead")
+	var diags diag.Diagnostics
+	c := m.(*service.NitroClient)
+
+	endpoint := "apidefs"
+
+	DefinitionName := d.Get("name").(string)
+
+	returnData, err := c.GetAllResource(endpoint)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// Find the correct resource with the given name and store Definition_id from the object
+	for _, v := range returnData[endpoint].([]interface{}) {
+		apigwDefinition := v.(map[string]interface{})
+		if apigwDefinition["name"].(string) == DefinitionName {
+			d.SetId(apigwDefinition["id"].(string))
+			d.Set("name", apigwDefinition["name"].(string))
+			break
+		}
+	}
+	if d.Id() == "" {
+		return diag.FromErr(fmt.Errorf("ApiGW Definition with name %s not found", DefinitionName))
+	}
+	return diags
+}
